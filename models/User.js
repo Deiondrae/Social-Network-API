6dd6c62b3f906b77a4f5a1a3bb597548704b9ed1
Deiondrae <deiondrae@gmail.com>
2022-07-16 16:24:51 -0400

    added user model

---
+++ models/User.js
@@ -0,0 +1,44 @@
+const { Schema, model } = require("mongoose");
+var isEmail = require("validator");
+
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            trim: true, 
+            unique: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            validate: [isEmail, "invalid email"]
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "thought"
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: user
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+});
+
+// UserSchema.virtual("friendcount").get(function() {
+//     return this.thoughts.reduce( =>)
+// })
+const User = model("User", UserSchema);
+
+module.exports = User;
